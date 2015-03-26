43a43eca177389cd91d47a477464f14148e5b241
Tao Wu <bjwt@linux.vnet.ibm.com>
2015-03-26 10:34:45 +0800

    add lvmFactoryTest.py
    Signed-off-by: Tao Wu <bjwt@linux.vnet.ibm.com>

---
+++ lvmFactoryTest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import logging
+import sys
+
+blivet_log = logging.getLogger("blivet")
+
+def set_up_logging():
+    """ Configure the blivet logger to use /tmp/blivet.log as its log file. """
+    blivet_log.setLevel(logging.DEBUG)
+    program_log = logging.getLogger("program")
+    program_log.setLevel(logging.DEBUG)
+    handler = logging.FileHandler("/tmp/blivet.log")
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s,%(msecs)03d %(levelname)s %(name)s: %(message)s")
+    handler.setFormatter(formatter)
+    blivet_log.addHandler(handler)
+    program_log.addHandler(handler)
+
+set_up_logging()
+
+import blivet
+b=blivet.Blivet()
+b.reset()
+
+sdc=b.devicetree.getDeviceByName("sdc")
+sdd=b.devicetree.getDeviceByName("sdd")
+disks=[sdc,sdd]
+
+for i in disks:
+  b.recursiveRemove(i)
+
+for i in disks:
+  b.initializeDisk(i)
+
+factory = blivet.devicefactory.LVMFactory(b, 550, disks, mountpoint = "/l1", fstype="ext4", name="rt", container_name="zkvm0001")
+factory.configure()
+
+b.doIt()
